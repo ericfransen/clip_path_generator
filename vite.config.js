3be537186ce1633ac099127198b371dfc51adaa6
Test User <test@example.com>
2026-02-01 23:45:12 -0700

    added deploy script to run github page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures relative paths for easy deployment/opening of dist/index.html
+  base: '/clip_path_generator/', // Update this to match your GitHub repository name
 })
